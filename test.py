3e2c6751a8f559f270f36a6371c658c9434a9e95
taeho0888 <taeho0888@naver.com>
2023-03-27 15:54:12 +0900

    대화 가능하게 코드 수정

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import openai
 import requests
+from datetime import datetime
 
 # .env 파일 로드
 load_dotenv()
@@ -16,11 +17,29 @@ headers = {
     "Content-Type": "application/json",
     "Authorization": f"Bearer {openai.api_key}",
 }
-data = {
-    "model": "gpt-3.5-turbo",
-    "messages": [{"role": "user", "content": "Hello!"}],
-}
-response = requests.post(url, headers=headers, json=data)
 
-# 결과 확인
-print(response.json())
+
+while True:
+    input_content = input("질문을 입력해주세요(나가기 : exit) : ")
+    if input_content == "exit":
+        break
+
+    data = {
+    "model": "gpt-3.5-turbo",
+    "messages": [{"role": "teacher", "content": input_content}],
+    }
+    
+    start_time = datetime.now()
+    response = requests.post(url, headers=headers, json=data)
+    end_time = datetime.now()
+    duration = end_time - start_time
+
+    response_json = response.json()
+    content = response_json["choices"][0]["message"]["content"]
+
+    with open("response.txt", "a+") as f:
+        f.write("\n\n[ USER ]\n")
+        f.write(input_content)
+        f.write("\n\n[ chatGPT ]\n")
+        f.write(content)
+        f.write(f"\n\n걸린 시간 : {duration.total_seconds()}초")
